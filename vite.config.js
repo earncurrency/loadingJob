c1f1cd1e5f9ae7f4fcd58362aa2d7463919f0991
kornwsn <kornwsn04@gmail.com>
2025-09-03 09:27:52 +0700

    update base part

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     tailwindcss(),
 
   ],
+  base: process.env.NODE_ENV === 'production' ? '/loadingJob/' : '',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
